5631136f2e81b4dcae7ea729947c07717397593c
domospb <d.artciukh@digitaldao.com>
2024-01-16 22:21:16 +0300

    format optional argument added

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
-#group = parser.add_mutually_exclusive_group()
-#group.add_argument("-v", "--verbose", action="store_true")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-f FORMAT", "--format FORMAT", action="store_true", help = "set format of output")
 #group.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("first_file")
 parser.add_argument("second_file")
@@ -13,7 +13,7 @@ answer = f'{args.first_file} {args.second_file}'
 def main():
     if args.quiet:
         print(answer)
-    elif args.verbose:
+    elif args.format:
         print(f"compare {args.first_file} and {args.second_file}")
     else:
         print(f"diff {args.first_file} {args.second_file} == {answer}")
